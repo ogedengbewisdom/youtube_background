a40bb9dac6904e48a5d4929e73308f9eee021a55
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2022-09-09 10:06:40 +0100

    background changer

---
+++ index.js
@@ -0,0 +1,21 @@
+let pinkEl = document.getElementById("pink");
+let purpleEl = document.getElementById("purple");
+let redEl = document.getElementById("red");
+
+
+
+function pink() {
+    document.querySelector("body").style.backgroundColor = "pink";
+    document.querySelector("body").style.color= "white"
+}
+
+function purple() {
+    document.querySelector("body").style.backgroundColor = "purple";
+    document.querySelector("body").style.color= "white"
+}
+
+function red() {
+    document.querySelector("body").style.backgroundColor = "red";
+    document.querySelector("body").style.color= "white"
+}
+
